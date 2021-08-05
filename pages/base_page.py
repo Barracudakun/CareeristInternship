281c6bcf2b8a2d1842fd2bba5dff08407c9ca010
Barracudakun <yingxukun@yahoo.com>
2021-08-04 23:58:51 -0500

    internship update

--- pages/base_page.py
+++ pages/base_page.py
@@ -15,6 +15,14 @@ class BasePage(object):
         def find_element(self, *locator):
             return self.driver.find_element(*locator)
 
+        def click(self, *locator):
+            self.driver.find_element(*locator).click()
+
+        def input_text(self, text, *locator):
+            e = self.driver.find_element(*locator)
+            e.clear()
+            e.send_keys(text)
+
         def open(self, url):
             url = self.base_url + url
             self.driver.get(url)
