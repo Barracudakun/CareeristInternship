f92ee0696100e5421770b7e3d3723a9fd1d88252
Yingxu Yao <yingxuyao@fb.com>
2021-08-09 17:02:20 -0500

    update Agu9

--- features/steps/add_to_cart.py
+++ features/steps/add_to_cart.py
@@ -1,21 +1,23 @@
 from behave import *
+from selenium.webdriver.common.by import By
 
 
 @given(u'Open Gettop page')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Given Open Gettop page')
+def open_main_page(context):
+    context.driver.get('https://gettop.us/')
 
 
 @when(u'Click on the first item from LATEST PRODUCTS ON SALE')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When Click on the first item from LATEST PRODUCTS ON SALE')
+def click_firstItem_on_lastProductOnSale(context):
+    context.driver.find_element(By.CSS_SELECTOR, '.product-title a').click()
+
 
 
 @when(u'Click on ADD TO CART')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: When Click on ADD TO CART')
+def click_on_ADDTOCART(context):
+    context.driver.find_element(By.CSS_SELECTOR, '.cart button').click()
 
 
 @then(u'Verify user can add product to cart')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then Verify user can add product to cart')
+def verify_add_product_to_cart(context):
+
